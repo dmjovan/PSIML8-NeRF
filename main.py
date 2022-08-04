68d8acf6b1c74b68a0d20b44fa45b8385d05b509
dmjovan <dmjovan@gmail.com>
2022-08-04 12:25:49 +0000

    ref1

--- main.py
+++ main.py
@@ -14,58 +14,102 @@ import time
 
 def train():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_file', type=str, default=r"C:\Users\psiml8\Desktop\Project\SSR\configs\SSR_room0_config.yaml", 
-                    help='config file name.')
-    parser.add_argument('--dataset_type', type=str, default="replica", choices= ["replica", "replica_nyu_cnn", "scannet"], 
-                        help='the dataset to be used,')
 
-    ### working mode and specific options
+    parser.add_argument("--config_file", 
+                        type=str, 
+                        default=r"C:\Users\psiml8\Desktop\Project\SSR\configs\room0_config.yaml", 
+                        help="Config file name.")
 
-    # sparse-views
-    parser.add_argument("--sparse_views", action='store_true',
-                        help='Use labels from a sparse set of frames')
-    parser.add_argument("--sparse_ratio", type=float, default=0,
-                        help='The portion of dropped labelling frames during training, which can be used along with all working modes.')    
-    parser.add_argument("--label_map_ids", nargs='*', type=int, default=[],
-                        help='In sparse view mode, use selected frame ids from sequences as supervision.')
-    parser.add_argument("--random_sample", action='store_true', help='Whether to randomly/evenly sample frames from the sequence.')
+    parser.add_argument("--dataset_type", 
+                        type=str, 
+                        default="replica", 
+                        choices= ["replica", "lego"], 
+                        help="The dataset to be used.")
 
-    # denoising---pixel-wsie
-    parser.add_argument("--pixel_denoising", action='store_true',
-                        help='Whether to work in pixel-denoising tasks.')
-    parser.add_argument("--pixel_noise_ratio", type=float, default=0,
-                        help='In sparse view mode, if pixel_noise_ratio > 0, the percentage of pixels to be perturbed in each sampled frame  for pixel-wise denoising task..')
+    ### WORKING MODE AND SPECIFIC OPTIONS ###
+
+    # Sparse views
+    parser.add_argument("--sparse_views", 
+                        action="store_true",
+                        help="Use labels from a sparse set of frames")
+
+    parser.add_argument("--sparse_ratio", 
+                        type=float, 
+                        default=0,
+                        help="The portion of dropped labelling frames during training, which can be used along with all working modes.")
+
+    parser.add_argument("--label_map_ids", 
+                        nargs="*", 
+                        type=int, 
+                        default=[],
+                        help="In sparse view mode, use selected frame ids from sequences as supervision.")
+
+    parser.add_argument("--random_sample", 
+                        action="store_true", 
+                        help="Whether to randomly or evenly sample frames from the sequence.")
+
+    # Denoising - Pixel-wise
+    parser.add_argument("--pixel_denoising", 
+                        action="store_true",
+                        help="Whether to work on pixel-denoising.")
+
+    parser.add_argument("--pixel_noise_ratio",
+                        type=float, 
+                        default=0,
+                        help="In sparse view mode, if pixel_noise_ratio > 0, the percentage of pixels to be perturbed in each sampled frame  for pixel-wise denoising.")
                         
-    # denoising---region-wsie
-    parser.add_argument("--region_denoising", action='store_true',
-                        help='Whether to work in region-denoising tasks by flipping class labels of chair instances in Replica Room_2')
-    parser.add_argument("--region_noise_ratio", type=float, default=0,
-                        help='In region-wise denoising task, region_noise_ratio is the percentage of chair instances to be perturbed in each sampled frame for region-wise denoising task.')
-    parser.add_argument("--uniform_flip", action='store_true',
-                        help='In region-wise denoising task, whether to change chair labels uniformly or not, i.e., by ascending area ratios. This corresponds to two set-ups mentioned in the paper.')
-    parser.add_argument("--instance_id", nargs='*', type=int, default=[3, 6, 7, 9, 11, 12, 13, 48],
-                        help='In region-wise denoising task, the chair instance ids in Replica Room_2 to be randomly perturbed. The ids of all 8 chairs are [3, 6, 7, 9, 11, 12, 13, 48]')
+    # Denoising - Region-wise
+    parser.add_argument("--region_denoising", 
+                        action="store_true",
+                        help="Whether to work in region-denoising tasks by flipping class labels of chair instances in Replica Room_2")
+
+    parser.add_argument("--region_noise_ratio", 
+                        type=float, 
+                        default=0,
+                        help="In region-wise denoising task, region_noise_ratio is the percentage of chair instances to be perturbed in each sampled frame for region-wise denoising.")
+    
+    parser.add_argument("--uniform_flip", 
+                        action="store_true",
+                        help="In region-wise denoising task, whether to change chair labels uniformly or not, i.e., by ascending area ratios. This corresponds to two set-ups mentioned in the paper.")
+    
+    parser.add_argument("--instance_id", 
+                        nargs="*", 
+                        type=int, 
+                        default=[3, 6, 7, 9, 11, 12, 13, 48],
+                        help="In region-wise denoising task, the chair instance ids in Replica Room_2 to be randomly perturbed. The ids of all 8 chairs are [3, 6, 7, 9, 11, 12, 13, 48]")
        
-    # super-resolution
-    parser.add_argument("--super_resolution", action='store_true',
-                        help='set to render synthetic data on a white bkgd (always use for dvoxels)')
-    parser.add_argument('--dense_sr',  action='store_true', help='Whether to use dense or sparse labels for SR instead of dense labels.')
-    parser.add_argument('--sr_factor',  type=int, default=8, help='Scaling factor of super-resolution.')
+    # Super-Resolution -> Image Upscaling and Sharpening 
+    parser.add_argument("--super_resolution", 
+                        action="store_true",
+                        help="Set to render synthetic data on a white bkgd (always use for dvoxels).")
 
-    # label propagation
-    parser.add_argument("--label_propagation", action='store_true',
-                        help='Label propagation using partial seed regions.')
-    parser.add_argument("--partial_perc", type=float, default=0,
-                        help='0: single-click propagation; 1: using 1-percent sub-regions for label propagation, 5: using 5-percent sub-regions for label propagation')
+    parser.add_argument("--dense_sr",  
+                        action="store_true", 
+                        help="Whether to use dense or sparse labels for Super-Resolution instead of dense labels.")
 
-    # misc.
-    parser.add_argument('--visualise_save',  action='store_true', help='whether to save the noisy labels into harddrive for later usage')
-    parser.add_argument('--load_saved',  action='store_true', help='use trained noisy labels for training to ensure consistency betwwen experiments')
-    parser.add_argument('--gpu', type=str, default="", help='GPU IDs.')
+    parser.add_argument("--sr_factor",  
+                        type=int, 
+                        default=8, 
+                        help="Scaling factor of Super-Resolution.")
+
+    # Label Propagation
+    parser.add_argument("--label_propagation", 
+                        action="store_true",
+                        help="Label propagation using partial seed regions.")
+
+    parser.add_argument("--partial_perc", 
+                        type=float,
+                        default=0,
+                        help="0: single-click propagation; 1: using 1-percent sub-regions for label propagation, 5: using 5-percent sub-regions for label propagation")
+
+    # Miscellaneous arguments
+    parser.add_argument("--visualise_save",  action="store_true", help="whether to save the noisy labels into harddrive for later usage")
+    parser.add_argument("--load_saved",  action="store_true", help="use trained noisy labels for training to ensure consistency betwwen experiments")
+    parser.add_argument("--gpu", type=str, default="", help="GPU IDs.")
 
     args = parser.parse_args()
     # Read YAML file
-    with open(args.config_file, 'r') as f:
+    with open(args.config_file, "r") as f:
         config = yaml.safe_load(f)
     if len(args.gpu)>0:
         config["experiment"]["gpu"] = args.gpu
@@ -202,7 +246,7 @@ def train():
     N_iters = int(float(config["train"]["N_iters"])) + 1
     global_step = start
     ##########################
-    print('Begin')
+    print("Begin")
     #####  Training loop  #####
     for i in trange(start, N_iters):
 
@@ -215,8 +259,8 @@ def train():
         global_step += 1
 
 
-    print('done')
+    print("done")
 
 
-if __name__=='__main__':
+if __name__=="__main__":
     train()
